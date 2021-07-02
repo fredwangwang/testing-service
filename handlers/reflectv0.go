3566d8d25a3b2f9bd5dc6059563f367f36d73849
Huan Wang <fredwanghuan@gmail.com>
2021-07-02 11:05:04 -0600

    reflect prints out the incoming request as well

--- handlers/reflectv0.go
+++ handlers/reflectv0.go
@@ -2,31 +2,34 @@ package handlers
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func ReflectV0(w http.ResponseWriter, r *http.Request) {
+	writer := io.MultiWriter(w, os.Stderr)
 	w.WriteHeader(200)
 
-	w.Write(b("Method:"))
-	w.Write(b(r.Method))
+	writer.Write(b("Method:"))
+	writer.Write(b(r.Method))
 
-	w.Write(b("\nHeaders:"))
+	writer.Write(b("\nHeaders:"))
 	for k, vs := range r.Header {
-		w.Write(b(fmt.Sprintf("%s: %#v", k, vs)))
+		writer.Write(b(fmt.Sprintf("%s: %#v", k, vs)))
 	}
 
-	w.Write(b("\nPath:"))
-	w.Write(b(r.URL.Path))
+	writer.Write(b("\nPath:"))
+	writer.Write(b(r.URL.Path))
 
-	w.Write(b("\nQuery Params:"))
+	writer.Write(b("\nQuery Params:"))
 	for k, vs := range r.URL.Query() {
-		w.Write(b(fmt.Sprintf("%s: %#v", k, vs)))
+		writer.Write(b(fmt.Sprintf("%s: %#v", k, vs)))
 	}
 
-	w.Write(b("\nBody:"))
+	writer.Write(b("\nBody:"))
 	defer r.Body.Close()
 	bodyContent, _ := ioutil.ReadAll(r.Body)
-	w.Write(bodyContent)
+	writer.Write(bodyContent)
 }
